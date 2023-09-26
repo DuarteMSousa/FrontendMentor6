a05edf90888b2d0c6d1358f73e8384e445f967f8
Duarte Sousa <126507544+DuarteMSousa@users.noreply.github.com>
2023-09-26 16:07:10 +0100

    a

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-        checkboxes.forEach(cb => {
-            if (cb !== checkbox) {
-                cb.checked = false;
-            }
-        });
-    });
-});
+function go_to_1() {
+    let toggleSwitch = document.getElementsByClassName('redButton')[0];
+    var r = document.querySelector(':root')
+    toggleSwitch.classList.add('position1');
+    toggleSwitch.classList.remove('position2');
+    toggleSwitch.classList.remove('position3');
+    r.style.setProperty('--mainBackground', 'hsl(222, 26%, 31%)')
+    r.style.setProperty('--toggleBackground', 'hsl(223, 31%, 20%)')
+    r.style.setProperty('--screenBackground', ' hsl(224, 36%, 15%)')
+    r.style.setProperty('--blueKeyBackground', 'hsl(225, 21%, 49%)')
+    r.style.setProperty('--blueKeyShadow', 'hsl(224, 28%, 35%)')
+    r.style.setProperty('--redKeyBackground', 'hsl(6, 63%, 50%)')
+    r.style.setProperty('--redKeyShadow', 'hsl(6, 70%, 34%)')
+    r.style.setProperty('--orangeKeyBackground', 'hsl(30, 25%, 89%)')
+    r.style.setProperty('--grayishOrangeKeyShadow', 'hsl(28, 16%, 65%)')
+    r.style.setProperty('--veryDarkGrayishBlue', 'hsl(221, 14%, 31%)')
+    r.style.setProperty('--white', 'hsl(0, 0%, 100%)')
+    r.style.setProperty('--resetHover', '#A2B3E1')
+    r.style.setProperty('--equalsHover', '#F96C5B')
+    r.style.setProperty('--textColor', 'hsl(0, 0%, 100%)')
+}
+
+function go_to_2() {
+    let toggleSwitch = document.getElementsByClassName('redButton')[0];
+    var r = document.querySelector(':root')
+    toggleSwitch.classList.add('position2');
+    toggleSwitch.classList.remove('position1');
+    toggleSwitch.classList.remove('position3');
+    r.style.setProperty('--mainBackground', 'hsl(268, 75%, 9%)')
+    r.style.setProperty('--toggleBackground', 'hsl(268, 71%, 12%)')
+    r.style.setProperty('--screenBackground', ' hsl(268, 71%, 12%)')
+    r.style.setProperty('--blueKeyBackground', 'hsl(281, 89%, 26%)')
+    r.style.setProperty('--blueKeyShadow', 'hsl(285, 91%, 52%)')
+    r.style.setProperty('--redKeyBackground', 'hsl(176, 100%, 44%)')
+    r.style.setProperty('--redKeyShadow', 'hsl(177, 92%, 70%)')
+    r.style.setProperty('--orangeKeyBackground', 'hsl(268, 47%, 21%)')
+    r.style.setProperty('--grayishOrangeKeyShadow', 'hsl(290, 70%, 36%)')
+    r.style.setProperty('--veryDarkGrayishBlue', 'hsl(52, 100%, 62%)')
+    r.style.setProperty('--white', 'hsl(0, 0%, 100%)')
+    r.style.setProperty('--resetHover', '#A2B3E1')
+    r.style.setProperty('--equalsHover', '#F96C5B')
+    r.style.setProperty('--textColor', 'hsl(52, 100%, 62%)')
+}
+
+function go_to_3() {
+    let toggleSwitch = document.getElementsByClassName('redButton')[0];
+    var r = document.querySelector(':root')
+    toggleSwitch.classList.add('position3');
+    toggleSwitch.classList.remove('position1');
+    toggleSwitch.classList.remove('position2');
+    r.style.setProperty('--mainBackground', 'hsl(0, 0%, 90%)')
+    r.style.setProperty('--toggleBackground', 'hsl(0, 5%, 81%)')
+    r.style.setProperty('--screenBackground', ' hsl(0, 0%, 93%)')
+    r.style.setProperty('--blueKeyBackground', 'hsl(185, 42%, 37%)')
+    r.style.setProperty('--blueKeyShadow', 'hsl(185, 58%, 25%)')
+    r.style.setProperty('--redKeyBackground', 'hsl(25, 98%, 40%)')
+    r.style.setProperty('--redKeyShadow', 'hsl(25, 99%, 27%)')
+    r.style.setProperty('--orangeKeyBackground', 'hsl(45, 7%, 89%)')
+    r.style.setProperty('--grayishOrangeKeyShadow', 'hsl(35, 11%, 61%)')
+    r.style.setProperty('--veryDarkGrayishBlue', 'hsl(60, 10%, 19%)')
+    r.style.setProperty('--white', 'hsl(0, 0%, 100%)')
+    r.style.setProperty('--resetHover', 'hsl(223, 31%, 20%)')
+    r.style.setProperty('--equalsHover', 'hsl(223, 31%, 20%)')
+    r.style.setProperty('--textColor', 'hsl(60, 10%, 19%)')
+
+
+    
+}
